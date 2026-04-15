5fda947b83ba7dba000e0295f52e2ce5ffc68b3e
Hector <hectorjuniorsanchez12@gmail.com>
2026-04-15 15:46:35 -0400

    Cambie faltas

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,11 +6,11 @@ const NavBar = () => {
   <header className="navbar bg-base-300 py-8 mb-10">
     <div className="w-full max-w-[1000px] mx-auto flex items-center justify-between">
         <NavLink className="text-3xl font-bold" to="/">
-            facegray
+            Facegray
         </NavLink>
         <NavLink className="btn btn-soft btn-primary font-bold text-[1.1em]" to="/createNote">
             <PlusIcon />
-                Crea una publicacion
+                Crea una publicación
         </NavLink>
 
     </div>
